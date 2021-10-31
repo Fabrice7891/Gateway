fa1f16287d8c61ffc7dda841d6fd456d42834253
fab7891 <fabrice_nokam@yahoo.fr>
2021-10-31 20:16:38 +0100

    Init commit

---
+++ src/main/java/com/example/geteway/GetewayApplication.java
@@ -0,0 +1,17 @@
+package com.example.geteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GetewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetewayApplication.class, args);
+	}
+
+}
